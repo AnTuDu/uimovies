619cbeb6f84350f0ad71c9ef542c8e3ca3d305b1
indu <70087467+Lazyindu@users.noreply.github.com>
2022-11-24 02:45:31 +0530

    done

--- Script.py
+++ Script.py
@@ -10,11 +10,11 @@ Here is the help for my COMMANDS."""
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Lazy Princess is an open source project. 
-- Source - https://github.com/LazyDeveloperr/LazyPrincess  
+- This is my private BOT. so i cant give source code.
+- All right reserved @Cm_Of_india 
 
-<b>DEVS:</b>
-- <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>"""
+<b>JOIN:</b>
+- <a href=https://t.me/techie_hub_chatx>Support Chat</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and LazyPriness will respond whenever that keyword hits the message
