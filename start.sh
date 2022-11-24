032dcdfbcaa6a27fa12a186c569189a3836ef3bd
AnTuDu <109011425+AnTuDu@users.noreply.github.com>
2022-11-24 07:54:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Lazyindu/uimovies.git /uimovies
+  git clone https://github.com/AnTuDu/uimovies.git /uimovies
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /uimovies
